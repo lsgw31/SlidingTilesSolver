3aa737ef2ca1a4423145be7305ca3695feb28350
Lucas Grenier <lsgw31@gmail.com>
2024-07-09 15:50:36 -0400

    Add place1() method
     - Puts 1 at coords (0, 0)
     - First of 4 steps in larger algorithm

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ class Puzzle:
         print('\nIf you entered your numbers correctly, this is what your puzzle should look like:')
         print('\033[1m' + '\n'.join(' '.join(str(j) if j else ' ' for j in i) for i in self.m) + '\033[0m')
 
-        # Put 1 in TL
+        self.place1()
         self.print_moves_and_puzzle()
 
-        # First set of 2 (2 & 3)
-        self.print_moves_and_puzzle()
+        # # First set of 2 (2 & 3)
+        # self.print_moves_and_puzzle()
 
-        # Second set of 2 (4 & 7) (Share a method with above?)
-        self.print_moves_and_puzzle()
+        # # Second set of 2 (4 & 7) (Share a method with above?)
+        # self.print_moves_and_puzzle()
 
-        # Rotate BL 2x2 till it's solved
-        self.print_moves_and_puzzle('solved')
+        # # Rotate BL 2x2 till it's solved
+        # self.print_moves_and_puzzle(solved = True)
     
-    def print_moves_and_puzzle(self, solved = None):
-        for i in range(1, random.randint(5, 20)):
-            self.moves.append(random.choice(tuple(MOVE_TRANSLATE.keys())))  # for testing so that it can make moves and dev can see UI
+    def print_moves_and_puzzle(self, *, solved = False):
+        # for i in range(1, random.randint(5, 20)):
+        #     self.moves.append(random.choice(tuple(MOVE_TRANSLATE.keys())))  # for testing so that it can make moves and dev can see UI
 
         print('\n\nNow make these moves:')
         print(', '.join(MOVE_TRANSLATE[i] for i in self.moves))
@@ -51,6 +51,25 @@ class Puzzle:
         d = len(direction) - 1
         for i in range(4):
             self.move0(ALL_2X2_ROTATIONS[coord][d][i], LOCATIONS_IN_ROTATIONS[coord][-d - (2 * d - 1) * i])
+    
+    def place1(self):
+        if self.m[2][1] == 1:  # put 0 in center; avoid putting 1 in BR
+            self.move0((-1, 0), (2, 2))
+            self.move0((0, -1), (1, 2))
+        else:
+            self.move0((0, -1), (2, 2))
+            self.move0((-1, 0), (2, 1))
+        
+        one = self.search(1)
+        if one in ((0, 1), (0, 2), (1, 2)):
+            direction = 'c'
+        else:
+            direction = 'cc'
+        
+        for sub_coord in ONE_ROTATIONS[direction][3 - one[0] - one[1]:]:
+            self.rotate2x2(sub_coord, direction)
+        
+        
 
 def calc(a, op, b):
     return tuple(map(eval, ALL_CALCS[op]))
@@ -63,7 +82,7 @@ MOVE_TRANSLATE = {
     (-1, 0): 'UP'
     }
 
-ALL_CALCS = {  # for calc() function; saves time/memory as local variables don't have to be continuously redefined
+ALL_CALCS = {
     '+': ('a[0] + b[0]', 'a[1] + b[1]'), 
     '-': ('a[0] - b[0]', 'a[1] - b[1]'), 
     '*': ('a[0] * b', 'a[1] * b'), 
@@ -91,7 +110,14 @@ LOCATIONS_IN_ROTATIONS = {
     
     (1, 0): ((1, 1), (2, 1), (2, 0), (1, 0), (1, 1)), 
     
-    (1, 1): ((1, 1), (1, 2), (2, 2), (2, 1), (1, 1)), 
+    (1, 1): ((1, 1), (1, 2), (2, 2), (2, 1), (1, 1))
+}
+
+
+ONE_ROTATIONS = {
+    'c': ((0, 1), (0, 1), (0, 0)),
+
+    'cc': ((1, 0), (1, 0), (0, 0))
 }
 
 Puzzle(input('\033[1m\nEnter your numbers here: \033[0m')).solve()
